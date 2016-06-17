a12a0c6d3615b4c12dbdc91bed7ec3140fe7b335
Epowerj <epowerj@gmail.com>
2016-06-17 21:30:17 +0000

    Added /say and commands.txt

--- bot.py
+++ bot.py
@@ -5,14 +5,28 @@ from key import apikey
 import datetime
 import random
 
-tribow_id=106890603
-adventure_id=12345
+tribow_id = 106890603
+adventure_id = -1001030866550
+
+savepath = "info.dict"
+info = {}
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+
+def load_info():
+    global info
+
+    save = open(savepath, 'r')
+    info = json.load(save)
+
+def save_info():
+    save = open(savepath, 'w')
+    json.dump(info, save)
 
 
 def start(bot, update):
@@ -46,7 +60,8 @@ def error(bot, update, error):
 
 def say(bot, update):
     if update.message.from_user.id == tribow_id:
-        bot.sendMessage(adventure_id, text=update.message.text)
+        commandtext = update.message.text.split(' ', 1)
+        bot.sendMessage(adventure_id, text=commandtext[1])
     else:
         bot.sendMessage(update.message.chat_id, text="You are not authorized")
 
@@ -61,6 +76,7 @@ def main():
     dp.add_handler(CommandHandler("time", time))
     dp.add_handler(CommandHandler("roll", roll))
     dp.add_handler(CommandHandler("chatinfo", chatinfo))
+    dp.add_handler(CommandHandler("say", say))
 
     dp.add_error_handler(error)
 
