79cece0455d3868a61788c88f21999b42ee362e8
Epowerj <epowerj@gmail.com>
2016-07-09 03:47:37 +0000

    Changed key to new bot

--- key.py
+++ key.py
@@ -1,2 +1,3 @@
 
-apikey = "234019611:AAEsMrI_SqCReEDnX6jfnxlOV5v_jLwTJmI"
+apikey = "237947594:AAGQ9nuPPYxoCrujbtAnxVKWHctbgaepdXM"
+#trobow apikey = "234019611:AAEsMrI_SqCReEDnX6jfnxlOV5v_jLwTJmI"
