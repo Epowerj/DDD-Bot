e2245baff8583bbb0e8e82e5f3b6b533aa08ee68
Epowerj <epowerj@gmail.com>
2016-10-01 19:59:52 -0400

    Updated bot

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ import logging
 from key import apikey, admin_id, chatroom_id
 import datetime, json, random, time
 
-savepath = "info.dict"
-info = {}
+char_info = {}
+char_info_savepath = "info.dict"
+
+next_action = {}
+next_action_savepath = "actions.dict"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,14 +19,21 @@ logger = logging.getLogger(__name__)
 
 
 def load_info():
-    global info
+    global char_info
+    global next_action
 
-    save = open(savepath, 'r')
-    info = json.load(save)
+    save = open(char_info_savepath, 'r')
+    char_info = json.load(save)
+
+    save = open(next_action_savepath, 'r')
+    next_action = json.load(save)
 
 def save_info():
-    save = open(savepath, 'w')
-    json.dump(info, save)
+    save = open(char_info_savepath, 'w')
+    json.dump(char_info, save)
+
+    save = open(next_action_savepath, 'w')
+    json.dump(next_action, save)
 
 
 def start(bot, update):
@@ -36,14 +46,14 @@ def help(bot, update):
 
 def ping(bot, update):
     bot.sendMessage(update.message.chat_id, text='Pong')
-    
+
 
 def time(bot, update):
     bot.sendMessage(update.message.chat_id, text=str(datetime.datetime.now()))
 
 
 def roll(bot, update):
-    bot.sendMessage(update.message.chat_id, reply_to_message_id=update.message.message_id, text=str(random.randint(1, 20)))
+   action(bot, update, random.randint(1, 20))
 
 
 def chatinfo(bot, update):
@@ -63,36 +73,87 @@ def say(bot, update):
         bot.sendMessage(update.message.chat_id, text="You are not authorized")
 
 
+def parse(bot, update):
+    print("Message from " + update.message.from_user.first_name + "(" + str(update.message.from_user.id) + "): " + update.message.text + " (" + str(update.message.message_id) + ")")
+
+
 def info(bot, update):
-    global info
+    global char_info
 
-    commandtext = update.message.text.split(' ', 1)[1]
+    commandtext = update.message.text.split(' ')
 
-    if commandtext in info:
-        global info
-        bot.sendMessage(update.message.chat_id, text=info[commandtext])
+    if len(commandtext) >= 2:
+        commandtext = commandtext[1].lower()
+
+        if commandtext in char_info:
+            bot.sendMessage(update.message.chat_id, text=char_info[commandtext])
+        else:
+            bot.sendMessage(update.message.chat_id, text="No info found on '"+commandtext+"'")
     else:
-        bot.sendMessage(update.message.chat_id, text="No info found on '"+commandtext+"'")
+        bot.sendMessage(update.message.chat_id, text="Usage: /info <topic>")
+
+
+def action(bot, update, roll=-1):
+    global next_action
+
+    commandtext = update.message.text.split(' ')
+
+    if len(commandtext) >= 2:
+        commandtext = update.message.text.split(' ', 1)[1]
+
+        if roll == -1:
+            next_action[str(update.message.from_user.id)] = update.message.from_user.first_name + ' - ' + commandtext
+        else:
+            next_action[str(update.message.from_user.id)] = update.message.from_user.first_name + ' - ' + str(roll) + ' - ' + commandtext
+
+        save_info()
+
+        bot.sendMessage(update.message.chat_id, reply_to_message_id=update.message.message_id, text="Action saved")
+    else:
+        bot.sendMessage(update.message.chat_id, reply_to_message_id=update.message.message_id, text="Your current action is: '" + next_action[str(update.message.from_user.id)] + "' \n Do /action <your next move> to update")
 
 
 def setinfo(bot, update):
-    global info
+    global char_info
 
     if update.message.from_user.id == admin_id:
         commandtext = update.message.text.split(' ', 2)
-        info[commandtext[1]] = commandtext[2]
+        char_info[commandtext[1].lower()] = commandtext[2]
 
         save_info()
+
+        bot.sendMessage(update.message.chat_id, text="Info saved")
+    else:
+        bot.sendMessage(update.message.chat_id, text="You are not authorized")
+
+
+def listactions(bot, update):
+    global next_action
+
+    if update.message.from_user.id == admin_id:
+        for action in next_action:
+            bot.sendMessage(update.message.chat_id, text=next_action[action])
+    else:
+        bot.sendMessage(update.message.chat_id, text="You are not authorized")
+
+
+def clearactions(bot, update):
+    global next_action
+
+    if update.message.from_user.id == admin_id:
+        for action in next_action:
+            next_action[action] = ''
     else:
         bot.sendMessage(update.message.chat_id, text="You are not authorized")
 
 
 def main():
-    global info
+    global char_info
+    global next_action
 
-    print(info)
     load_info()
-    print(info)
+    print(char_info)
+    print(next_action)
 
     updater = Updater(apikey)
     dp = updater.dispatcher
@@ -105,7 +166,10 @@ def main():
     dp.add_handler(CommandHandler("chatinfo", chatinfo))
     dp.add_handler(CommandHandler("say", say))
     dp.add_handler(CommandHandler("info", info))
+    dp.add_handler(CommandHandler("action", action))
     dp.add_handler(CommandHandler("setinfo", setinfo))
+    dp.add_handler(CommandHandler("listactions", listactions))
+    dp.add_handler(CommandHandler("clearactions", clearactions))
 
     dp.add_handler(MessageHandler([Filters.text], parse))
 
