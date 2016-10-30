03662088953a0761006a3b0fb0c9ba59e2e8bd41
Epowerj <epowerj@gmail.com>
2016-10-30 17:36:08 -0400

    updated key.py to work with environment variables

--- key.py
+++ key.py
@@ -1,4 +1,6 @@
+import os
 
-apikey = "bot toke goes here"
-admin_id = "user id of the admin goes here"
-chatroom_id = "chat id of the chatroom goes here"
+#you can replace the environment variable calls with normal strings
+apikey = str(os.environ["DDDKEY"])
+admin_id = int(os.environ["DDDADMIN"])
+chatroom_id = int(os.environ["DDDCHAT"])
