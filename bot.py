c850b27210f9f04230b44ceb654810ea2ae171e1
Epowerj <epowerj@gmail.com>
2016-11-24 00:07:48 +0000

    Changed existence check

--- bot.py
+++ bot.py
@@ -35,10 +35,8 @@ def db_connect():
     #check if table for the app exists or not
     cur.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '" + table_name + "');")
 
-    print(cur.fetchone())
-
     #TODO fix if statement vvv
-    if not (bool(cur.rowcount)): #if it doesn't exist
+    if (cur.fetchone()) #if it doesn't exist
         print("Table not found, creating new one")
         cur.execute("CREATE TABLE " + table_name + " (id serial PRIMARY KEY, info varchar, data varchar);")
 
