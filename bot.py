8f7f2bc5445008efbc719354da0b6b7c8635e9fc
Epowerj <epowerj@gmail.com>
2016-12-05 23:22:26 -0500

    Re-added json code

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ from key import apikey, admin_id, chatroom_id, table_name
 from urllib.parse import urlparse
 import os, logging, datetime, json, random, time
 
-conn = False
-cur = False
+char_info = {}
+char_info_savepath = "info.dict"
+
+next_action = {}
+next_action_savepath = "actions.dict"
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,8 +18,22 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def add_info():
-    cur.execute("INSERT INTO ")
+def load_info():
+    global char_info
+    global next_action
+
+    save = open(char_info_savepath, 'r')
+    char_info = json.load(save)
+
+    save = open(next_action_savepath, 'r')
+    next_action = json.load(save)
+
+def save_info():
+    save = open(char_info_savepath, 'w')
+    json.dump(char_info, save)
+
+    save = open(next_action_savepath, 'w')
+    json.dump(next_action, save)
 
 
 def start(bot, update):
@@ -74,18 +91,9 @@ def info(bot, update):
     if len(commandtext) >= 2:
         commandtext = commandtext[1].lower()
 
-        cur.execute("SELECT EXISTS (SELECT 1 FROM " + table_name + " where info = '" + commandtext + "')")
-        result = cur.fetchone()
-        print(result)
-        
-        if (result[0]):
-            print("Info exists")
-            cur.execute("SELECT data FROM " + table_name + " where info = '" + commandtext + "'")
-            result = cur.fetchone()
-            print(result)
-            bot.sendMessage(update.message.chat_id, text=result)
+        if commandtext in char_info:
+            bot.sendMessage(update.message.chat_id, text=char_info[commandtext])
         else:
-            print("Info not found")
             bot.sendMessage(update.message.chat_id, text="No info found on '"+commandtext+"'")
     else:
         bot.sendMessage(update.message.chat_id, text="Usage: /info <topic>")
@@ -104,7 +112,7 @@ def action(bot, update, roll=-1):
         else:
             next_action[str(update.message.from_user.id)] = update.message.from_user.first_name + ' - ' + str(roll) + ' - ' + commandtext
 
-        #save_info()
+        save_info()
 
         if roll == -1:
             bot.sendMessage(update.message.chat_id, reply_to_message_id=update.message.message_id, text="Action saved")
@@ -117,25 +125,15 @@ def action(bot, update, roll=-1):
         bot.sendMessage(update.message.chat_id, reply_to_message_id=update.message.message_id, text="Your current action is: '" + next_action[str(update.message.from_user.id)] + "' \n Do /action <your next move> to update")
 
 
+
 def setinfo(bot, update):
     global char_info
 
     if update.message.from_user.id == admin_id:
         commandtext = update.message.text.split(' ', 2)
-        #char_info[commandtext[1].lower()] = commandtext[2]
+        char_info[commandtext[1].lower()] = commandtext[2]
 
-        cur.execute("SELECT EXISTS (SELECT 1 FROM " + table_name + " where info = '" + commandtext[1] + "')")
-
-        if not (cur.fetchone()):
-            print("Creating info entry...")
-            cur.execute("INSERT INTO " + table_name + " (info, data) VALUES (" + commandtext[1].lower() + ", " + commandtext[2] + ")")
-            global conn
-            conn.commit()
-        else:
-            print("Updating info entry...")
-            cur.execute("UPDATE " + table_name + " SET data='" + commandtext[2] + "' WHERE info='" + commandtext[1].lower() + "'")
-            global conn
-            conn.commit()
+        save_info()
 
         bot.sendMessage(update.message.chat_id, text="Info saved")
     else:
@@ -149,8 +147,7 @@ def listactions(bot, update):
         for action in next_action:
             bot.sendMessage(update.message.chat_id, text=next_action[action])
     else:
-        bot.sendMessage(update.message.chat_id, text="You are not authorized")
-
+            bot.sendMessage(update.message.chat_id, text="You are not authorized")
 
 def clearactions(bot, update):
     global next_action
@@ -163,6 +160,13 @@ def clearactions(bot, update):
 
 
 def main():
+    global char_info
+    global next_action
+
+    load_info()
+    print(char_info)
+    print(next_action)
+
     TOKEN = apikey
     PORT = int(os.environ.get('PORT', '5000'))
     updater = Updater(TOKEN)
