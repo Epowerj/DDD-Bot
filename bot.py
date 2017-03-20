d261bf2964ede2854292bd224cb805d9a76d4998
Epowerj <epowerj@gmail.com>
2017-03-20 02:43:01 -0400

    Added /equips and /stats

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from pymongo import MongoClient
 db = 0
 char_info = {}
 next_action = {}
+char_stats = {}
+char_equips = {}
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,6 +22,8 @@ logger = logging.getLogger(__name__)
 def load_info():
     global char_info
     global next_action
+    global char_stats
+    global char_equips
     global db
 
     client = MongoClient(str(os.environ["MONGODB_URI"]))  # connect to the server
@@ -27,11 +31,17 @@ def load_info():
 
     char_collection = db.charinfo  # select collection
     action_collection = db.actions
+    stats_collection = db.stats
+    equips_collection = db.equips
 
     char_info = char_collection.find_one()
     print(char_info)
     next_action = action_collection.find_one()
     print(next_action)
+    char_stats = stats_collection.find_one()
+    print(char_stats)
+    char_equips = equips_collection.find_one()
+    print(char_equips)
 
     if char_info is None:
         char_info = {}
@@ -39,14 +49,24 @@ def load_info():
     if next_action is None:
         next_action = {}
 
+    if char_stats is None:
+        char_stats = {}
+
+    if char_equips is None:
+        char_equips = {}
+
 
 def save_info():
     global char_info
     global next_action
+    global char_stats
+    global char_equips
     global db
 
     db.charinfo.update({}, char_info, upsert=True)
     db.actions.update({}, next_action, upsert=True)
+    db.stats.update({}, char_stats, upsert=True)
+    db.equips.update({}, char_equips, upsert=True)
 
 
 def start(bot, update):
@@ -154,6 +174,38 @@ def info(bot, update):
         bot.sendMessage(update.message.chat_id, text="Usage: /info <topic>")
 
 
+def stats(bot, update):
+    global char_stats
+
+    commandtext = update.message.text.split(' ')
+
+    if len(commandtext) >= 2:
+        commandtext = commandtext[1].lower()
+
+        if commandtext in char_stats:
+            bot.sendMessage(update.message.chat_id, text=char_stats[commandtext])
+        else:
+            bot.sendMessage(update.message.chat_id, text="No info found on '"+commandtext+"'")
+    else:
+        bot.sendMessage(update.message.chat_id, text="Usage: /info <topic>")
+
+
+def equips(bot, update):
+    global char_equips
+
+    commandtext = update.message.text.split(' ')
+
+    if len(commandtext) >= 2:
+        commandtext = commandtext[1].lower()
+
+        if commandtext in char_equips:
+            bot.sendMessage(update.message.chat_id, text=char_equips[commandtext])
+        else:
+            bot.sendMessage(update.message.chat_id, text="No info found on '"+commandtext+"'")
+    else:
+        bot.sendMessage(update.message.chat_id, text="Usage: /info <topic>")
+
+
 def action(bot, update, roll=-1):
     global next_action
 
@@ -190,6 +242,36 @@ def setinfo(bot, update):
         commandtext = update.message.text.split(' ', 2)
 
         char_info[commandtext[1].lower()] = commandtext[2]
+
+        save_info()
+
+        bot.sendMessage(update.message.chat_id, text="Info saved")
+    else:
+        bot.sendMessage(update.message.chat_id, text="You are not authorized")
+
+
+def setstats(bot, update):
+    global char_stats
+
+    if update.message.from_user.id == admin_id:  # if admin
+        commandtext = update.message.text.split(' ', 2)
+
+        char_stats[commandtext[1].lower()] = commandtext[2]
+
+        save_info()
+
+        bot.sendMessage(update.message.chat_id, text="Info saved")
+    else:
+        bot.sendMessage(update.message.chat_id, text="You are not authorized")
+
+
+def setequips(bot, update):
+    global char_equips
+
+    if update.message.from_user.id == admin_id:  # if admin
+        commandtext = update.message.text.split(' ', 2)
+
+        char_equips[commandtext[1].lower()] = commandtext[2]
 
         save_info()
 
@@ -250,8 +332,12 @@ def main():
     dp.add_handler(CommandHandler("say", say))
     dp.add_handler(CommandHandler("qsay", qsay))
     dp.add_handler(CommandHandler("info", info))
+    dp.add_handler(CommandHandler("stats", stats))
+    dp.add_handler(CommandHandler("equips", equips))
     dp.add_handler(CommandHandler("action", action))
     dp.add_handler(CommandHandler("setinfo", setinfo))
+    dp.add_handler(CommandHandler("setstats", setstats))
+    dp.add_handler(CommandHandler("setequips", setequips))
     dp.add_handler(CommandHandler("listactions", listactions))
     dp.add_handler(CommandHandler("clearactions", clearactions))
 
